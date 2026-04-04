d7e9b07733e806e3fb4a0ededae43fb41f79e9fc
MD-Kayesur <rmdkayesur@gmail.com>
2026-04-04 14:37:57 +0600

    add MicroCredential component to landing page

--- src/components/landingPages/LandingPage.tsx
+++ src/components/landingPages/LandingPage.tsx
@@ -3,11 +3,13 @@
 import { Hero } from "@/components/landingPages/Hero";
 import { Features } from "@/components/Features";
 import { HowItWorks } from "@/components/landingPages/HowItWorks";
+import { MicroCredential } from "@/components/landingPages/MicroCredential";
 
 export default function LandingPage() {
     return (
         <main className="min-h-screen bg-background">
             <Hero />
+            <MicroCredential />
             {/* <Features /> */}
             <HowItWorks />
         </main>
