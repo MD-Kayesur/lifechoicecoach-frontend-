082a2367b5a4a49fa02090d1d5e26ba9b1c99d57
MD-Kayesur <rmdkayesur@gmail.com>
2026-04-16 14:17:52 +0600

    move neuroscience components to a centralized directory and update imports

--- src/components/landingPages/Neuroscience.tsx
+++ src/components/landingPages/Neuroscience.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { NeuroscienceHero } from "./Neuroscience/NeuroscienceHero";
-import { StartingPoint } from "./Neuroscience/StartingPoint";
-import { Practitioners } from "./Neuroscience/Practitioners";
-import { BrainScience } from "./Neuroscience/BrainScience";
-import { LearningLoop } from "./Neuroscience/LearningLoop";
-import { ConceptAttainment } from "./Neuroscience/ConceptAttainment";
-import { IntegrityEngine } from "./Neuroscience/IntegrityEngine";
-import { SkillPromise } from "./Neuroscience/SkillPromise";
+import { NeuroscienceHero } from "../Neuroscience/NeuroscienceHero";
+import { StartingPoint } from "../Neuroscience/StartingPoint";
+import { Practitioners } from "../Neuroscience/Practitioners";
+import { BrainScience } from "../Neuroscience/BrainScience";
+import { LearningLoop } from "../Neuroscience/LearningLoop";
+import { ConceptAttainment } from "../Neuroscience/ConceptAttainment";
+import { IntegrityEngine } from "../Neuroscience/IntegrityEngine";
+import { SkillPromise } from "../Neuroscience/SkillPromise";
 
 export const Neuroscience = () => {
     return (
